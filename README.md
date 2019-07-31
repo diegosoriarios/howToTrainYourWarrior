1ba4aebd765a2085962b218d7c82e9c0c5943aba
diegosoriarios <diegosoriarios@gmail.com>
2019-07-31 17:28:42 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+npm install http-server -g
+http-server -c-1 -p 8010
---
+++ game.js
@@ -0,0 +1,193 @@
+let game;
+let gameOptions = {
+    gravity: 1,
+    maxItemsPerLevel: 30,
+    maxIterations: 10,
+    minItemsDistance: 160
+}
+
+let score = 0
+let play = false
+let totalLifes = 5
+let lives = totalLifes
+
+const HERO = 0;
+const COIN = 1;
+const SKULL = 2;
+
+window.onload = function() {
+    let gameConfig = {
+        type: Phaser.AUTO,
+        scale: {
+            mode: Phaser.Scale.FIT,
+            autoCenter: Phaser.Scale.CENTER_BOTH,
+            parent: "thegame",
+            width: 750,
+            height: 1334
+        },
+        scene: [playGame, Menu],
+        //scene: [Menu, playGame], //Final
+        physics: {
+            default: "matter",
+            matter: {
+                gravity: {
+                    y: gameOptions.gravity
+                }
+            }
+        }
+    }
+    game = new Phaser.Game(gameConfig);
+    window.focus();
+}
+
+class Menu extends Phaser.Scene {
+    constructor() {
+        super("Menu")
+    }
+
+    create() {
+        console.log(game)
+        let nome = this.add.text((game.canvas.width / 2) - 248, 150, 'NOME DO JOGO', { fontFamily: 'Arial', fontSize: 64, color: '#fff' });
+        console.log(nome.width)
+        this.playText = this.add.text((game.canvas.width / 2) - 248, game.canvas.height / 2, 'Play', { fontFamily: 'Arial', fontSize: 64, color: '#fff' });
+        this.playText.setInteractive(new Phaser.Geom.Rectangle(0, 0, this.playText.width, this.playText.height), Phaser.Geom.Rectangle.Contains);
+        this.playText.on("pointerdown", this.startGame);
+    }
+
+    startGame() {
+        console.log('aqui')
+        play = true
+    }
+    
+    update() {
+        if(play) {
+            this.scene.start("PlayGame");
+        }
+    }
+}
+
+class playGame extends Phaser.Scene{
+    constructor(){
+        super("PlayGame");
+    }
+    preload(){
+        this.load.spritesheet("items", "items.png", {
+            frameWidth: 128,
+            frameHeight: 128
+        });
+    }
+    create(){
+        this.score = this.add.text(10, 10, `Score ${score}`, { fontFamily: 'Arial', fontSize: 32, color: '#fff' });
+        this.lives = this.add.text(10, 50, `Lives ${lives}`, { fontFamily: 'Arial', fontSize: 32, color: '#fff' });
+        this.canSummonHero = true;
+        this.matter.world.update30Hz();
+        this.matter.world.setBounds(0, -400, game.config.width, game.config.height + 800);
+        this.createLevel();
+        this.input.on("pointerdown", this.releaseHero, this);
+        this.matter.world.on("collisionstart", function(e, b1, b2){
+            switch(b1.label){
+                case COIN:
+                    b1.gameObject.visible = false;
+                    this.matter.world.remove(b1);
+                    score++
+                    console.log(score, lives)
+                    break;
+                case SKULL:
+                    if(b1.gameObject.y > b2.gameObject.y){
+                        b1.gameObject.visible = false;
+                        this.matter.world.remove(b1);
+                        this.cameras.main.flash(50, 255, 0, 0);
+                    }
+                    else{
+                        this.cameras.main.flash(50, 255, 0, 0);
+                    }
+                    lives--
+                    console.log(score, lives)
+                    break;
+                default:
+                    if(b2.gameObject.y > game.config.height){
+                        this.scene.start("PlayGame");
+                    }
+                    else{
+                        if(b2.gameObject.y > 0){
+                            this.cameras.main.flash(50, 255, 0, 0);
+                        }
+                    }
+            }
+            this.update()
+        }, this);
+    }
+
+    createLevel(){
+        this.gameItems = this.add.group();
+        let spawnRectangle = new Phaser.Geom.Rectangle(80, 250, game.config.width - 160, game.config.height - 350);
+        for(let i = 0; i < gameOptions.maxItemsPerLevel; i++){
+            let iterations = 0;
+            let point;
+            do{
+                point = Phaser.Geom.Rectangle.Random(spawnRectangle);
+                iterations ++;
+			} while(iterations < gameOptions.maxIterations && this.itemsOverlap(point));
+            if(iterations == gameOptions.maxIterations){
+                break;
+            }
+            else{
+                let item = this.matter.add.image(point.x, point.y, "items");
+                item.setCircle();
+                item.setStatic(true);
+                this.gameItems.add(item);
+                if(Phaser.Math.Between(0, 99) > 50){
+                    item.setFrame(1);
+                    item.body.label = COIN;
+                }
+                else{
+                    item.setFrame(2);
+                    item.body.label = SKULL;
+                }
+            }
+        }
+    }
+
+    itemsOverlap(p){
+        let overlap = false;
+        this.gameItems.getChildren().forEach(function(item){
+            if(item.getCenter().distance(p) < gameOptions.minItemsDistance){
+                overlap = true
+            }
+        })
+		return overlap;
+    }
+    
+    releaseHero(e){
+        if(this.canSummonHero){
+            this.canSummonHero = false;
+            let item = this.matter.add.image(e.x, -200, "items");
+            item.setCircle();
+            item.setBounce(1)
+            item.body.label = HERO
+        }
+    }
+
+    
+    render() {
+        this.game.debug.text(`Debugging Phaser ${Phaser.VERSION}`, 20, 20, 'yellow', 'Segoe UI');
+    }
+
+    update() {
+        this.score.destroy()
+        this.lives.destroy()
+        this.score = this.add.text(10, 10, `Score ${score}`, { fontFamily: 'Arial', fontSize: 32, color: '#fff' });
+        this.lives = this.add.text(10, 50, `Lives ${lives}`, { fontFamily: 'Arial', fontSize: 32, color: '#fff' });
+        if(lives === 0) {
+            //this.create()
+            play = false
+            lives = totalLifes
+            this.scene.start("Menu");
+        }
+    }
+};
+
+
+class TrainingScreen extends Phaser.Scene {
+
+}
